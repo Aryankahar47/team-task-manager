fb717c446b12528ff9c1cee549a0ad69fe74cb21
Aryan Kahar <aryankahar463@gmail.com>
2026-05-11 19:43:27 +0530

    Database added

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
